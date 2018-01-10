551eb5f43a5f061dbc698304d1ea6836a03f1544
= <a.widzinski@gmail.com>
2018-01-10 20:06:15 +0100

    Zapisywanie wyników do Calculations podczas ładowania

--- src/main/java/com/widzin/bootstrap/SpringJpaBootstrap.java
+++ src/main/java/com/widzin/bootstrap/SpringJpaBootstrap.java
@@ -12,7 +12,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +38,6 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
     private PlayersAndClubLoadService playersAndClubLoadService;
 
 	private Logger log = Logger.getLogger(SpringJpaBootstrap.class);
-	private SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
 
 	//private final int MATCHES_IN_ONE_SEASON = 34;
 
@@ -105,39 +103,23 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-//        loadService = new MainLoadService();
-//        matchesLoadService = new MatchesLoadService();
-//        playersAndClubLoadService = new PlayersAndClubLoadService();
-//
-//        loadPlayers();
-//        loadMatches();
-//        loadLogos();
-//        saveToDatabase();
-//        //loadLogos();
-//    	/*loadClubs();
-//		loadMatches(2015, 2016);
-//		loadMatches(2016, 2017);*/
-//        loadUsers();
-//        loadRoles();
-//        assignUsersToDefaultRoles();
-		/*calculations = Calculations.getInstance();
-		for (Game g: gameService.findAllGames()) {
-			log.info("Sprawdzam mecz nr. " + g.getId());
-			try {
-				if (g.getDate().after(ft.parse("2016-07-10")) && g.isPlayed()) {
-					calculations.addAllGoalsScoredAtHome(g.getHomeScore());
-					calculations.addAllGoalsLostAtHome(g.getAwayScore());
-					calculations.addNumberOfAllMatches();
-				}
-			} catch (ParseException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-		log.info("Wszystkie bramki strzelone: " + calculations.getAllGoalsScoredAtHome());
-		log.info("Wszystkie bramki stracone: " + calculations.getAllGoalsLostAtHome());*/
+        loadService = new MainLoadService();
+        matchesLoadService = new MatchesLoadService();
+        playersAndClubLoadService = new PlayersAndClubLoadService();
+
+        loadPlayers();
+        loadMatches();
+        loadLogos();
+        saveToDatabase();
+        loadUsers();
+        loadRoles();
+        assignUsersToDefaultRoles();
+        setCalculationsOnLoad();
 	}
 
-	private void loadPlayers() {
+
+
+    private void loadPlayers() {
         playersAndClubLoadService.startParsing(Links.PLAYERS_2015_2016, loadService);
         playersAndClubLoadService.startParsing(Links.PLAYERS_2016_2017, loadService);
         playersAndClubLoadService.startParsing(Links.PLAYERS_2017_2018, loadService);
@@ -197,108 +179,6 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
         log.info("Saved teamMatchDetails id: " + teamMatchDetails.getId());
     }
 
-    /*private void loadMatches(int from, int to){
-		String path = "src\\main\\resources\\static\\data\\Terminarz_" + from + "_" + to + ".data";
-		try {
-			File file = new File(path);
-
-			String content = new String(Files.readAllBytes(file.toPath()));
-			String[] matchdays = content.split("Kolejka");
-			String[] newMatchdays = new String[matchdays.length - 1];
-
-			for (int i = 0; i < newMatchdays.length; i++) {
-				newMatchdays[i] = matchdays[i + 1];
-			}
-
-			for (String m: newMatchdays) {
-				String[] enter = m.split("\n");
-				for (int i = 0; i < (enter.length - 1); i++) {
-					enter[i] = enter[i + 1];
-				}
-
-				String[] newEnters = new String[enter.length - 1];
-				for (int i = 0; i < newEnters.length; i++) {
-					newEnters[i] = enter[i].trim();
-				}
-				addMatchToJavaClass(newEnters);
-			}
-		} catch (Exception e) {
-			log.info("Cannot find file");
-		}
-	}
-
-	private void addMatchToJavaClass(String[] strings) {
-		Date date = new Date();
-
-		for (String s: strings) {
-			if (s.length() > 0 ) {
-				if (Character.isDigit(s.charAt(0))) {
-					String[] temp = s.split(":");
-					String[] dates = temp[0].split("\\.");
-					int day = Integer.parseInt(dates[0]);
-					int month = Integer.parseInt(dates[1]);
-					int year = Integer.parseInt(dates[2]);
-					try {
-						date = ft.parse(year + "-" + month + "-" + day);
-					} catch (ParseException e) {
-						System.out.println("Cannot convert to date");
-					}
-				} else {
-					String[] details = s.split(" ");
-					Club home = findByPieceOfName(details[0]);
-					Club away = findByPieceOfName(details[2]);
-					String[] score = details[3].split(":");
-					int homeScore = Integer.parseInt(score[0]);
-					int awayScore = Integer.parseInt(score[1]);
-					Game match = new Game(home, away, homeScore, awayScore, date);
-					gameService.saveMatch(match);
-					log.info("Saved match nr. " + match.getId());
-					updateClubs(match);
-				}
-			}
-		}
-	}
-
-	private Club findByPieceOfName(String piece) {
-		for (Club c: clubService.listAllClubs()) {
-			if (c.getName().contains(piece)) {
-				return c;
-			}
-		}
-		return null;
-	}*/
-
-	private void updateClubs(Game match) {
-		for (Club c: clubService.listAllClubs()) {
-			if (c.getName().equals(match.getHome().getName())) {
-				c.addGameAtHome(match);
-			} else if (c.getName().equals(match.getAway().getName())) {
-				c.addGameAway(match);
-			}
-		}
-
-		/*Club home = clubService.getClubById(match.getHome().getId());
-		Club away = clubService.getClubById(match.getAway().getId());
-		if (match.getHomeScore() > match.getAwayScore()){
-			home.addWins();
-			away.addLoses();
-		} else if (match.getHomeScore() < match.getAwayScore()) {
-			home.addLoses();
-			away.addWins();
-		} else {
-			home.addDraws();
-			away.addDraws();
-		}
-		home.addScoredGoals(match.getHomeScore());
-		home.addLostGoals(match.getAwayScore());
-		away.addScoredGoals(match.getAwayScore());
-		away.addLostGoals(match.getHomeScore());
-		home.updateStats();
-		away.updateStats();
-		clubService.saveClub(home);
-		clubService.saveClub(away);*/
-    }
-
     private void loadUsers() {
         loadUser("admin");
         loadUser("user");
@@ -343,6 +223,24 @@ public class SpringJpaBootstrap implements ApplicationListener<ContextRefreshedE
                 });
             }
         });
+    }
+
+    private void setCalculationsOnLoad() {
+        calculations = Calculations.getInstance();
+        for (Season season: loadService.getSeasons()) {
+            if (!season.getPeriod().startsWith("2015")) {
+                for (Match match: season.getMatches()) {
+                    log.info("Sprawdzam mecz nr. " + match.getId());
+                    if (match.isPlayed()) {
+                        calculations.addAllGoalsScoredAtHome(match.getHome().getGoals());
+                        calculations.addAllGoalsLostAtHome(match.getAway().getGoals());
+                        calculations.addNumberOfAllMatches();
+                    }
+                }
+            }
+        }
+//		log.info("Wszystkie bramki strzelone: " + calculations.getAllGoalsScoredAtHome());
+//		log.info("Wszystkie bramki stracone: " + calculations.getAllGoalsLostAtHome());
     }
 }
 
